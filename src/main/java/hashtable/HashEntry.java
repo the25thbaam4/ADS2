39c84cbb6d00fd513098ba9491d0a52133abd03d
the25thbaam4 <the25thbaam@tutanota.com>
2023-06-29 13:02:39 +0200

    Initial commit

---
+++ src/main/java/hashtable/HashEntry.java
@@ -0,0 +1,10 @@
+package hashtable;
+
+public class HashEntry {
+    private String key;
+    private int value;
+    public HashEntry(String key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
---
+++ src/main/java/hashtable/HashTable.java
@@ -0,0 +1,28 @@
+package hashtable;
+
+public class HashTable {
+    private int TABLE_SIZE;
+    private int size;
+    private HashEntry[] table;
+    private int primeSize;
+
+    public int getTABLE_SIZE() {
+        return TABLE_SIZE;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public HashEntry[] getTable() {
+        return table;
+    }
+
+    public int getPrimeSize() {
+        return primeSize;
+    }
+
+
+
+
+}
